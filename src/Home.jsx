3877bb40ddc6f0340cc86a1feaace4248f1447d5
Kyle Pazdel <kyle.kayvaun@gmail.com>
2022-11-03 11:32:43 -0700

    Adds dummy student data and passes through props to ResumesIndex.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,40 @@
 import { ResumesIndex } from "./ResumesIndex";
 
 export function Home() {
+  const students = [
+    {
+      first_name: "Robyn",
+      id: 1,
+      last_name: "Spaulding",
+      email: "r@test.com",
+      phone_number: "1-111-111-1111",
+      bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas posuere, tellus quis pharetra dapibus, nibh risus porttitor odio, at dictum velit nunc in dolor. Maecenas tellus tortor, rutrum varius auctor sed, bibendum eget ligula. Maecenas commodo dictum ex vitae aliquet. Aliquam interdum dui et nulla commodo, eget bibendum enim porttitor. Aenean ultricies cursus nisl, ut elementum enim imperdiet nec. Nam metus massa, viverra eget quam id, efficitur tempor augue. Vestibulum nec elit quam.",
+      linkedin_url: "linkedin.com/test",
+      twitter_handle: "@test",
+      website_url: "test.com",
+      online_resume_url: "resume.com",
+      github_url: "github.com/test",
+      photo: "...",
+    },
+    {
+      first_name: "Test",
+      id: 2,
+      last_name: "Test",
+      email: "test@test.com",
+      phone_number: "1-111-111-1111",
+      bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas posuere, tellus quis pharetra dapibus, nibh risus porttitor odio, at dictum velit nunc in dolor. Maecenas tellus tortor, rutrum varius auctor sed, bibendum eget ligula. Maecenas commodo dictum ex vitae aliquet. Aliquam interdum dui et nulla commodo, eget bibendum enim porttitor. Aenean ultricies cursus nisl, ut elementum enim imperdiet nec. Nam metus massa, viverra eget quam id, efficitur tempor augue. Vestibulum nec elit quam.",
+      linkedin_url: "linkedin.com/test",
+      twitter_handle: "@test",
+      website_url: "test.com",
+      online_resume_url: "resume.com",
+      github_url: "github.com/test",
+      photo: "...",
+    },
+  ];
+
   return (
     <div>
-      <ResumesIndex />
+      <ResumesIndex students={students} />
     </div>
   );
 }
